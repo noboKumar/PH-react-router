4baa44bf28ad6c06c1cb9a140e7f190ab8164d0f
noboKumar <nobokumar227@gmail.com>
2025-04-16 00:02:33 +0600

    add use navigation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 const Post = ({ post }) => {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate(`/posts/${post.id}`);
+  };
+
   return (
     <div className="border-2 p-5 rounded-xl space-y-5">
       <h1 className="text-xl">Post: {post.title}</h1>
       <Link className="btn" to={`/posts/${post.id}`}>
         Show Post
-      </Link>
+      </Link> <br/>
+      <button onClick={handleNavigate} className="btn">
+        show details of -{post.id}
+      </button>
     </div>
   );
 };
